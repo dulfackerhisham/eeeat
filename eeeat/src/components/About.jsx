aeec94da9bcfe38fa8929a33781189cc4a93b295
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-07-09 14:20:33 +0530

    created class based components & used props

--- eeeat/src/components/About.jsx
+++ eeeat/src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User.jsx"
+import UserClass from "../class/UserClass.jsx";
+
 const About = () => {
     return (
     <div>
         <h1>About us</h1>
         <p>this is about us page</p>
+        <User name={"izaan (function based)"}/>
+        <UserClass name={"jacky (class based)"} location={"india,kerala"}/>
     </div>
     );
 };
---
+++ eeeat/src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className="user-container">
+            <h2>Name: Hisham Dulfacker</h2>
+            <h3>Occupation: Software Developer</h3>
+            <h4>Location: Calicut</h4>
+        </div>
+    )
+}
+
+export default User;

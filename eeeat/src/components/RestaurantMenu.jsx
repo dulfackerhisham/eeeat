43a667f9922cde0b962d2aeae788324daeeb929e
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-07-10 17:41:21 +0530

    customHook created

--- eeeat/src/components/RestaurantMenu.jsx
+++ eeeat/src/components/RestaurantMenu.jsx
@@ -1,27 +1,15 @@
-import { useState ,useEffect } from "react"
 import Shimmer from "./Shimmer"
-import {MENU_API_URL} from "../utils/constants"
 import {useParams} from "react-router-dom"
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 
 const RestaurantMenu = () => {
 
-    const [resInfo, setResInfo] = useState(null);
 
     const {resId} = useParams();
     // console.log(resId);
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
-
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API_URL + resId);
-
-        const json = await data.json();
-        // console.log(json);
-        setResInfo(json)
-    };
+    const resInfo = useRestaurantMenu(resId);
 
     if (resInfo === null) return <Shimmer />;
 
---
+++ eeeat/src/utils/useRestaurantMenu.jsx
@@ -0,0 +1,23 @@
+// this hook takes a restaurant ID and returns restaurant informantion
+
+import { useState, useEffect } from "react";
+import { MENU_API_URL } from "./constants";
+
+const useRestaurantMenu = (resId) => {
+    // fetchdata 
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch(MENU_API_URL + resId);
+        const json = await data.json();
+        setResInfo(json)
+    }
+
+    return resInfo;
+}
+
+export default useRestaurantMenu
